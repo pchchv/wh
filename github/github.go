f731732b11b57d0128fe556b3661bcb56ada5a0a
pchchv <ipchchv@gmail.com>
2025-09-03 20:41:50 +0300

    Implement Parse method for Webhook to handle GitHub events and payload verification

--- github/github.go
+++ github/github.go
@@ -1,6 +1,16 @@
 package github
 
-import "errors"
+import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/hex"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+)
 
 const (
 	// GitHub hook types.
@@ -85,6 +95,247 @@ func New(options ...Option) (*Webhook, error) {
 		}
 	}
 	return hook, nil
+}
+
+// Parse verifies and parses the events specified and returns the payload object or an error.
+func (hook Webhook) Parse(r *http.Request, events ...Event) (interface{}, error) {
+	defer func() {
+		_, _ = io.Copy(io.Discard, r.Body)
+		_ = r.Body.Close()
+	}()
+
+	if len(events) == 0 {
+		return nil, errors.New("no Event specified to parse")
+	}
+
+	if r.Method != http.MethodPost {
+		return nil, errors.New("invalid HTTP Method")
+	}
+
+	event := r.Header.Get("X-GitHub-Event")
+	if event == "" {
+		return nil, errors.New("missing X-GitHub-Event Header")
+	}
+	gitHubEvent := Event(event)
+
+	var found bool
+	for _, evt := range events {
+		if evt == gitHubEvent {
+			found = true
+			break
+		}
+	}
+
+	// event not defined to be parsed
+	if !found {
+		return nil, errors.New("event not defined to be parsed")
+	}
+
+	payload, err := io.ReadAll(r.Body)
+	if err != nil || len(payload) == 0 {
+		return nil, errors.New("error parsing payload")
+	}
+
+	// If we have a Secret set, we should check the MAC
+	if len(hook.secret) > 0 {
+		signature := r.Header.Get("X-Hub-Signature-256")
+		if len(signature) == 0 {
+			return nil, errors.New("missing X-Hub-Signature-256 Header")
+		}
+
+		signature = strings.TrimPrefix(signature, "sha256=")
+		mac := hmac.New(sha256.New, []byte(hook.secret))
+		_, _ = mac.Write(payload)
+		expectedMAC := hex.EncodeToString(mac.Sum(nil))
+		if !hmac.Equal([]byte(signature), []byte(expectedMAC)) {
+			return nil, errors.New("HMAC verification failed")
+		}
+	}
+
+	switch gitHubEvent {
+	case CheckRunEvent:
+		var pl CheckRunPayload
+		err = json.Unmarshal([]byte(payload), &pl)
+		return pl, err
+	case CheckSuiteEvent:
+		var pl CheckSuitePayload
+		err = json.Unmarshal([]byte(payload), &pl)
+		return pl, err
+	case CommitCommentEvent:
+		var pl CommitCommentPayload
+		err = json.Unmarshal([]byte(payload), &pl)
+		return pl, err
+	case CreateEvent:
+		var pl CreatePayload
+		err = json.Unmarshal([]byte(payload), &pl)
+		return pl, err
+	case DeployKeyEvent:
+		var pl DeployKeyPayload
+		err = json.Unmarshal([]byte(payload), &pl)
+		return pl, err
+	case DeleteEvent:
+		var pl DeletePayload
+		err = json.Unmarshal([]byte(payload), &pl)
+		return pl, err
+	case DependabotAlertEvent:
+		var pl DependabotAlertPayload
+		err = json.Unmarshal([]byte(payload), &pl)
+		return pl, err
+	case DeploymentEvent:
+		var pl DeploymentPayload
+		err = json.Unmarshal([]byte(payload), &pl)
+		return pl, err
+	case DeploymentStatusEvent:
+		var pl DeploymentStatusPayload
+		err = json.Unmarshal([]byte(payload), &pl)
+		return pl, err
+	case ForkEvent:
+		var pl ForkPayload
+		err = json.Unmarshal([]byte(payload), &pl)
+		return pl, err
+	case GollumEvent:
+		var pl GollumPayload
+		err = json.Unmarshal([]byte(payload), &pl)
+		return pl, err
+	case InstallationEvent, IntegrationInstallationEvent:
+		var pl InstallationPayload
+		err = json.Unmarshal([]byte(payload), &pl)
+		return pl, err
+	case InstallationRepositoriesEvent, IntegrationInstallationRepositoriesEvent:
+		var pl InstallationRepositoriesPayload
+		err = json.Unmarshal([]byte(payload), &pl)
+		return pl, err
+	case IssueCommentEvent:
+		var pl IssueCommentPayload
+		err = json.Unmarshal([]byte(payload), &pl)
+		return pl, err
+	case IssuesEvent:
+		var pl IssuesPayload
+		err = json.Unmarshal([]byte(payload), &pl)
+		return pl, err
+	case LabelEvent:
+		var pl LabelPayload
+		err = json.Unmarshal([]byte(payload), &pl)
+		return pl, err
+	case MemberEvent:
+		var pl MemberPayload
+		err = json.Unmarshal([]byte(payload), &pl)
+		return pl, err
+	case MembershipEvent:
+		var pl MembershipPayload
+		err = json.Unmarshal([]byte(payload), &pl)
+		return pl, err
+	case MetaEvent:
+		var pl MetaPayload
+		err = json.Unmarshal([]byte(payload), &pl)
+		return pl, err
+	case MilestoneEvent:
+		var pl MilestonePayload
+		err = json.Unmarshal([]byte(payload), &pl)
+		return pl, err
+	case OrganizationEvent:
+		var pl OrganizationPayload
+		err = json.Unmarshal([]byte(payload), &pl)
+		return pl, err
+	case OrgBlockEvent:
+		var pl OrgBlockPayload
+		err = json.Unmarshal([]byte(payload), &pl)
+		return pl, err
+	case PageBuildEvent:
+		var pl PageBuildPayload
+		err = json.Unmarshal([]byte(payload), &pl)
+		return pl, err
+	case PingEvent:
+		var pl PingPayload
+		err = json.Unmarshal([]byte(payload), &pl)
+		return pl, err
+	case ProjectCardEvent:
+		var pl ProjectCardPayload
+		err = json.Unmarshal([]byte(payload), &pl)
+		return pl, err
+	case ProjectColumnEvent:
+		var pl ProjectColumnPayload
+		err = json.Unmarshal([]byte(payload), &pl)
+		return pl, err
+	case ProjectEvent:
+		var pl ProjectPayload
+		err = json.Unmarshal([]byte(payload), &pl)
+		return pl, err
+	case PublicEvent:
+		var pl PublicPayload
+		err = json.Unmarshal([]byte(payload), &pl)
+		return pl, err
+	case PullRequestEvent:
+		var pl PullRequestPayload
+		err = json.Unmarshal([]byte(payload), &pl)
+		return pl, err
+	case PullRequestReviewEvent:
+		var pl PullRequestReviewPayload
+		err = json.Unmarshal([]byte(payload), &pl)
+		return pl, err
+	case PullRequestReviewCommentEvent:
+		var pl PullRequestReviewCommentPayload
+		err = json.Unmarshal([]byte(payload), &pl)
+		return pl, err
+	case PushEvent:
+		var pl PushPayload
+		err = json.Unmarshal([]byte(payload), &pl)
+		return pl, err
+	case ReleaseEvent:
+		var pl ReleasePayload
+		err = json.Unmarshal([]byte(payload), &pl)
+		return pl, err
+	case RepositoryEvent:
+		var pl RepositoryPayload
+		err = json.Unmarshal([]byte(payload), &pl)
+		return pl, err
+	case RepositoryVulnerabilityAlertEvent:
+		var pl RepositoryVulnerabilityAlertPayload
+		err = json.Unmarshal([]byte(payload), &pl)
+		return pl, err
+	case SecurityAdvisoryEvent:
+		var pl SecurityAdvisoryPayload
+		err = json.Unmarshal([]byte(payload), &pl)
+		return pl, err
+	case StatusEvent:
+		var pl StatusPayload
+		err = json.Unmarshal([]byte(payload), &pl)
+		return pl, err
+	case TeamEvent:
+		var pl TeamPayload
+		err = json.Unmarshal([]byte(payload), &pl)
+		return pl, err
+	case TeamAddEvent:
+		var pl TeamAddPayload
+		err = json.Unmarshal([]byte(payload), &pl)
+		return pl, err
+	case WatchEvent:
+		var pl WatchPayload
+		err = json.Unmarshal([]byte(payload), &pl)
+		return pl, err
+	case WorkflowDispatchEvent:
+		var pl WorkflowDispatchPayload
+		err = json.Unmarshal([]byte(payload), &pl)
+		return pl, err
+	case WorkflowJobEvent:
+		var pl WorkflowJobPayload
+		err = json.Unmarshal([]byte(payload), &pl)
+		return pl, err
+	case WorkflowRunEvent:
+		var pl WorkflowRunPayload
+		err = json.Unmarshal([]byte(payload), &pl)
+		return pl, err
+	case GitHubAppAuthorizationEvent:
+		var pl GitHubAppAuthorizationPayload
+		err = json.Unmarshal([]byte(payload), &pl)
+		return pl, err
+	case CodeScanningAlertEvent:
+		var pl CodeScanningAlertPayload
+		err = json.Unmarshal([]byte(payload), &pl)
+		return pl, err
+	default:
+		return nil, fmt.Errorf("unknown event %s", gitHubEvent)
+	}
 }
 
 // WebhookOptions is a namespace for configuration option methods.
