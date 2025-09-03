12ad77bd60a48561e0e0ee62553d7f8c26844945
pchchv <ipchchv@gmail.com>
2025-09-03 20:43:59 +0300

    Add test setup for Webhook with secret option

---
+++ github/github_test.go
@@ -0,0 +1,21 @@
+package github
+
+import (
+	"log"
+	"os"
+	"testing"
+)
+
+var hook *Webhook
+
+func TestMain(m *testing.M) {
+	// setup
+	var err error
+	hook, err = New(Options.Secret("IsWishesWereHorsesWedAllBeEatingSteak!"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	os.Exit(m.Run())
+	// teardown
+}
