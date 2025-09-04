424fcd44c108f4fbc4aaef281d9195b125472454
pchchv <ipchchv@gmail.com>
2025-09-04 20:47:57 +0300

    Add DeploymentEventPayload type for GitLab's triggered when a deployment

--- gitlab/payload.go
+++ gitlab/payload.go
@@ -13,3 +13,20 @@ type SystemHookPayload struct {
 	EventName  string `json:"event_name"`
 	ObjectKind string `json:"object_kind"`
 }
+
+// DeploymentEventPayload contains the information for GitLab's triggered when a deployment.
+type DeploymentEventPayload struct {
+	User           User    `json:"user"`
+	Project        Project `json:"project"`
+	Status         string  `json:"status"`
+	UserUrl        string  `json:"user_url"`
+	ShortSha       string  `json:"short_sha"`
+	CommitUrl      string  `json:"commit_url"`
+	ObjectKind     string  `json:"object_kind"`
+	Environment    string  `json:"environment"`
+	CommitTitle    string  `json:"commit_title"`
+	DeployableUrl  string  `json:"deployable_url"`
+	StatusChangeAt string  `json:"status_changed_at"`
+	DeployableId   int64   `json:"deployable_id"`
+	DeploymentId   int64   `json:"deployment_id"`
+}
