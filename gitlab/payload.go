51ebd5710b84bfe9d250612728f6c2528bc206f5
pchchv <ipchchv@gmail.com>
2025-09-04 20:45:39 +0300

    Add SystemHookPayload type to match with real hook events

--- gitlab/payload.go
+++ gitlab/payload.go
@@ -7,3 +7,9 @@ type Reviewers struct {
 	Username  string `json:"username"`
 	AvatarURL string `json:"avatar_url"`
 }
+
+// SystemHookPayload contains the ObjectKind to match with real hook events.
+type SystemHookPayload struct {
+	EventName  string `json:"event_name"`
+	ObjectKind string `json:"object_kind"`
+}
