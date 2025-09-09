711febfff7b62a87c3a29424339bd6098a63ece4
pchchv <ipchchv@gmail.com>
2025-09-09 19:10:14 +0300

    Add Event type to define GitLab hook event by X-Gitlab-Event Header

--- gitlab/gitlab.go
+++ gitlab/gitlab.go
@@ -1,5 +1,8 @@
 package gitlab
 
+// Event defines a GitLab hook event type by the X-Gitlab-Event Header.
+type Event string
+
 // Webhook instance contains all methods needed to process events.
 type Webhook struct {
 	secretHash []byte
