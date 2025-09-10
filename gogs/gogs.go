810cb891d24a4d12706b50a3430567c9052bb781
pchchv <ipchchv@gmail.com>
2025-09-10 20:31:23 +0300

    Add an Event type defining the Gogs hook event type.

--- gogs/gogs.go
+++ gogs/gogs.go
@@ -1,5 +1,8 @@
 package gogs
 
+// Event defines a Gogs hook event type.
+type Event string
+
 // Webhook instance contains all methods needed to process events.
 type Webhook struct {
 	secret string
