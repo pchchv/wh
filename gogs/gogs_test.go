6f5491adcb5a1517c485efc125a0aac2cb91580d
pchchv <ipchchv@gmail.com>
2025-09-10 20:42:32 +0300

    Add main test to setup a secret

--- gogs/gogs_test.go
+++ gogs/gogs_test.go
@@ -1,1 +1,21 @@
 package gogs
+
+import (
+	"log"
+	"os"
+	"testing"
+)
+
+var hook *Webhook
+
+func TestMain(m *testing.M) {
+	// setup
+	var err error
+	hook, err = New(Options.Secret("sampleToken!"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	os.Exit(m.Run())
+	//teardown
+}
