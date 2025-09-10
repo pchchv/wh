edf80862bfafc34bc8e30d469650be5e155dab34
pchchv <ipchchv@gmail.com>
2025-09-10 20:32:32 +0300

    Add Gogs hook types

--- gogs/gogs.go
+++ gogs/gogs.go
@@ -1,5 +1,17 @@
 package gogs
 
+const (
+	// Gogs hook types
+	PushEvent         Event = "push"
+	ForkEvent         Event = "fork"
+	CreateEvent       Event = "create"
+	DeleteEvent       Event = "delete"
+	IssuesEvent       Event = "issues"
+	ReleaseEvent      Event = "release"
+	PullRequestEvent  Event = "pull_request"
+	IssueCommentEvent Event = "issue_comment"
+)
+
 // Event defines a Gogs hook event type.
 type Event string
 
