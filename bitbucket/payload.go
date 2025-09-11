090883e7d20fb605f453752fcecaad6d859fb943
pchchv <ipchchv@gmail.com>
2025-09-11 10:22:20 +0300

    Add Owner type, this is a common Bitbucket owner sub entity

--- bitbucket/payload.go
+++ bitbucket/payload.go
@@ -1,1 +1,21 @@
 package bitbucket
+
+// Owner is the common Bitbucket Owner Sub Entity.
+type Owner struct {
+	UUID        string `json:"uuid"`
+	Type        string `json:"type"`
+	NickName    string `json:"nickname"`
+	AccountID   string `json:"account_id"`
+	DisplayName string `json:"display_name"`
+	Links       struct {
+		Self struct {
+			Href string `json:"href"`
+		} `json:"self"`
+		HTML struct {
+			Href string `json:"href"`
+		} `json:"html"`
+		Avatar struct {
+			Href string `json:"href"`
+		} `json:"avatar"`
+	} `json:"links"`
+}
