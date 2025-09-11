9f6f1126c35ea1381c925b6eff576a2f091fed96
pchchv <ipchchv@gmail.com>
2025-09-11 10:30:05 +0300

    Add the RepoPushPayload type, payload data Bitbucket repo:push

--- bitbucket/payload.go
+++ bitbucket/payload.go
@@ -1,5 +1,7 @@
 package bitbucket
 
+import "time"
+
 // Owner is the common Bitbucket Owner Sub Entity.
 type Owner struct {
 	UUID        string `json:"uuid"`
@@ -117,4 +119,127 @@ type RepoUpdatedPayload struct {
 			Old string `json:"old"`
 		} `json:"full_name"`
 	} `json:"changes"`
+}
+
+// RepoPushPayload is the Bitbucket repo:push payload.
+type RepoPushPayload struct {
+	Actor      Owner      `json:"actor"`
+	Repository Repository `json:"repository"`
+	Push       struct {
+		Changes []struct {
+			New struct {
+				Type   string `json:"type"`
+				Name   string `json:"name"`
+				Target struct {
+					Type    string    `json:"type"`
+					Hash    string    `json:"hash"`
+					Author  Owner     `json:"author"`
+					Message string    `json:"message"`
+					Date    time.Time `json:"date"`
+					Parents []struct {
+						Type  string `json:"type"`
+						Hash  string `json:"hash"`
+						Links struct {
+							Self struct {
+								Href string `json:"href"`
+							} `json:"self"`
+							HTML struct {
+								Href string `json:"href"`
+							} `json:"html"`
+						} `json:"links"`
+					} `json:"parents"`
+					Links struct {
+						Self struct {
+							Href string `json:"href"`
+						} `json:"self"`
+						HTML struct {
+							Href string `json:"href"`
+						} `json:"html"`
+					} `json:"links"`
+				} `json:"target"`
+				Links struct {
+					Self struct {
+						Href string `json:"href"`
+					} `json:"self"`
+					Commits struct {
+						Href string `json:"href"`
+					} `json:"commits"`
+					HTML struct {
+						Href string `json:"href"`
+					} `json:"html"`
+				} `json:"links"`
+			} `json:"new"`
+			Old struct {
+				Type   string `json:"type"`
+				Name   string `json:"name"`
+				Target struct {
+					Type    string    `json:"type"`
+					Hash    string    `json:"hash"`
+					Author  Owner     `json:"author"`
+					Message string    `json:"message"`
+					Date    time.Time `json:"date"`
+					Parents []struct {
+						Type  string `json:"type"`
+						Hash  string `json:"hash"`
+						Links struct {
+							Self struct {
+								Href string `json:"href"`
+							} `json:"self"`
+							HTML struct {
+								Href string `json:"href"`
+							} `json:"html"`
+						} `json:"links"`
+					} `json:"parents"`
+					Links struct {
+						Self struct {
+							Href string `json:"href"`
+						} `json:"self"`
+						HTML struct {
+							Href string `json:"href"`
+						} `json:"html"`
+					} `json:"links"`
+				} `json:"target"`
+				Links struct {
+					Self struct {
+						Href string `json:"href"`
+					} `json:"self"`
+					Commits struct {
+						Href string `json:"href"`
+					} `json:"commits"`
+					HTML struct {
+						Href string `json:"href"`
+					} `json:"html"`
+				} `json:"links"`
+			} `json:"old"`
+			Links struct {
+				HTML struct {
+					Href string `json:"href"`
+				} `json:"html"`
+				Diff struct {
+					Href string `json:"href"`
+				} `json:"diff"`
+				Commits struct {
+					Href string `json:"href"`
+				} `json:"commits"`
+			} `json:"links"`
+			Created bool `json:"created"`
+			Forced  bool `json:"forced"`
+			Closed  bool `json:"closed"`
+			Commits []struct {
+				Hash    string `json:"hash"`
+				Type    string `json:"type"`
+				Message string `json:"message"`
+				Author  Owner  `json:"author"`
+				Links   struct {
+					Self struct {
+						Href string `json:"href"`
+					} `json:"self"`
+					HTML struct {
+						Href string `json:"href"`
+					} `json:"html"`
+				} `json:"links"`
+			} `json:"commits"`
+			Truncated bool `json:"truncated"`
+		} `json:"changes"`
+	} `json:"push"`
 }
