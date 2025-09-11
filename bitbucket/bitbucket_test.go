c2e054d866e01b0c5b6d6a2e4177ee9badc0b5ec
pchchv <ipchchv@gmail.com>
2025-09-11 10:56:17 +0300

    test(bitbucket): add test harness with Webhook initialization
    - Implement TestMain to initialize Webhook with UUID option
    - Provide newServer helper for creating test HTTP servers
    - Establish common path constant (/webhooks) for test endpoints

--- bitbucket/bitbucket_test.go
+++ bitbucket/bitbucket_test.go
@@ -1,1 +1,31 @@
 package bitbucket
+
+import (
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+)
+
+const path = "/webhooks"
+
+var hook *Webhook
+
+func TestMain(m *testing.M) {
+	// setup
+	var err error
+	hook, err = New(Options.UUID("MY_UUID"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	os.Exit(m.Run())
+	// teardown
+}
+
+func newServer(handler http.HandlerFunc) *httptest.Server {
+	mux := http.NewServeMux()
+	mux.HandleFunc(path, handler)
+	return httptest.NewServer(mux)
+}
