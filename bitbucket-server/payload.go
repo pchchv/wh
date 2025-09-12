c63b6480720d0dd833bb9a9ae96478c47c3e0de1
pchchv <ipchchv@gmail.com>
2025-09-12 11:36:11 +0300

    feat(bitbucket_server): add Comment struct for webhook payloads
    - Define Comment type with metadata (ID, version, timestamps, text)
    - Embed User as Author for proper user mapping
    - Support nested tasks, comments, properties, and permitted operations
    - Provide JSON tags aligned with Bitbucket Server API

--- bitbucket-server/payload.go
+++ bitbucket-server/payload.go
@@ -10,3 +10,16 @@ type User struct {
 	EmailAddress string                 `json:"emailAddress"`
 	Links        map[string]interface{} `json:"links"`
 }
+
+type Comment struct {
+	ID                  uint64                   `json:"id"`
+	Version             uint64                   `json:"version"`
+	CreatedDate         uint64                   `json:"createdDate"`
+	UpdatedDate         uint64                   `json:"updatedDate"`
+	Author              User                     `json:"author"`
+	Text                string                   `json:"text"`
+	Tasks               []map[string]interface{} `json:"tasks"`
+	Comments            []map[string]interface{} `json:"comments"`
+	Properties          map[string]interface{}   `json:"properties,omitempty"`
+	PermittedOperations map[string]interface{}   `json:"permittedOperations,omitempty"`
+}
