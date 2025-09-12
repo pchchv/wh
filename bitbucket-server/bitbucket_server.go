a867961de327de3bf7d52a410966b1eb3b1f5c7b
pchchv <ipchchv@gmail.com>
2025-09-12 11:55:56 +0300

    feat(bitbucket_server): define Event type and constants for webhook events
    - Add Event type alias for string
    - Introduce constants for repository events (forked, modified, refs, comments)
    - Introduce constants for pull request events (opened, merged, deleted, modified, declined, comments, reviewers, from_ref updates)
    - Include diagnostics ping event

--- bitbucket-server/bitbucket_server.go
+++ bitbucket-server/bitbucket_server.go
@@ -1,1 +1,26 @@
 package bitbucket_server
+
+const (
+	DiagnosticsPingEvent                 Event = "diagnostics:ping"
+	RepositoryForkedEvent                Event = "repo:forked"
+	RepositoryModifiedEvent              Event = "repo:modified"
+	RepositoryCommentAddedEvent          Event = "repo:comment:added"
+	RepositoryCommentEditedEvent         Event = "repo:comment:edited"
+	RepositoryCommentDeletedEvent        Event = "repo:comment:deleted"
+	RepositoryReferenceChangedEvent      Event = "repo:refs_changed"
+	PullRequestOpenedEvent               Event = "pr:opened"
+	PullRequestMergedEvent               Event = "pr:merged"
+	PullRequestDeletedEvent              Event = "pr:deleted"
+	PullRequestModifiedEvent             Event = "pr:modified"
+	PullRequestDeclinedEvent             Event = "pr:declined"
+	PullRequestCommentAddedEvent         Event = "pr:comment:added"
+	PullRequestCommentEditedEvent        Event = "pr:comment:edited"
+	PullRequestCommentDeletedEvent       Event = "pr:comment:deleted"
+	PullRequestReviewerUpdatedEvent      Event = "pr:reviewer:updated"
+	PullRequestReviewerApprovedEvent     Event = "pr:reviewer:approved"
+	PullRequestReviewerNeedsWorkEvent    Event = "pr:reviewer:needs_work"
+	PullRequestReviewerUnapprovedEvent   Event = "pr:reviewer:unapproved"
+	PullRequestFromReferenceUpdatedEvent Event = "pr:from_ref_updated"
+)
+
+type Event string
