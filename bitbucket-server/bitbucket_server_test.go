e7fa4568a84e9e10c614d89462dd7ee59d4ac9d3
pchchv <ipchchv@gmail.com>
2025-09-12 12:25:21 +0300

    feat(bitbucket_server): setup global webhook instance for tests
    - Initialize a Webhook with a secret before running tests
    - Use TestMain to handle setup and teardown

--- bitbucket-server/bitbucket_server_test.go
+++ bitbucket-server/bitbucket_server_test.go
@@ -1,1 +1,23 @@
 package bitbucket_server
+
+import (
+	"log"
+	"os"
+	"testing"
+)
+
+const path = "/webhooks"
+
+var hook *Webhook
+
+func TestMain(m *testing.M) {
+	// setup
+	var err error
+	hook, err = New(Options.Secret("secret"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	os.Exit(m.Run())
+	// teardown
+}
