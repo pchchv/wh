57f015130e5941832611d98ec9d6a0f404ce8a6a
pchchv <ipchchv@gmail.com>
2025-09-15 10:18:29 +0300

    test(docker): add TestMain setup for Webhook

--- docker/docker_test.go
+++ docker/docker_test.go
@@ -1,1 +1,21 @@
 package docker
+
+import (
+	"log"
+	"os"
+	"testing"
+)
+
+var hook *Webhook
+
+func TestMain(m *testing.M) {
+	// setup
+	var err error
+	hook, err = New()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	os.Exit(m.Run())
+	// teardown
+}
