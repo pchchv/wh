1962ed108417380b315b73f5f9125fa7482114c9
pchchv <ipchchv@gmail.com>
2025-09-15 10:54:09 +0300

    feat(azure): define Azure DevOps webhook event types
    - Adds `Event` type to represent Azure DevOps webhook event types.
    - Introduces constants for common events:
      - `git.push`
      - `build.complete`
      - `git.pullrequest.merged`
      - `git.pullrequest.created`
      - `git.pullrequest.updated`

--- azure/azure.go
+++ azure/azure.go
@@ -1,1 +1,13 @@
 package azure
+
+const (
+	// Azure DevOps Server hook types.
+	GitPushEventType               Event = "git.push"
+	BuildCompleteEventType         Event = "build.complete"
+	GitPullRequestMergedEventType  Event = "git.pullrequest.merged"
+	GitPullRequestCreatedEventType Event = "git.pullrequest.created"
+	GitPullRequestUpdatedEventType Event = "git.pullrequest.updated"
+)
+
+// Event defines an Azure DevOps server hook event type.
+type Event string
