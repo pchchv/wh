0dac602bda230eed2d824f04c72c86e5526cd999
pchchv <ipchchv@gmail.com>
2025-09-15 11:06:22 +0300

    feat(azure): add basic auth verification to Webhook
    - Adds `verifyBasicAuth` method to the `Webhook` struct.
    - Validates incoming HTTP requests using basic authentication.
    - Skips validation if `username` and `password` are not set.

--- azure/azure.go
+++ azure/azure.go
@@ -1,6 +1,8 @@
 // azure devops does not send an event header, this BasicEvent is provided to get the EventType.
 package azure
 
+import "net/http"
+
 const (
 	// Azure DevOps Server hook types.
 	GitPushEventType               Event = "git.push"
@@ -17,4 +19,14 @@ type Event string
 type Webhook struct {
 	username string
 	password string
+}
+
+func (hook Webhook) verifyBasicAuth(r *http.Request) bool {
+	if hook.username == "" && hook.password == "" {
+		// skip validation if username or password was not provided
+		return true
+	}
+
+	username, password, ok := r.BasicAuth()
+	return ok && username == hook.username && password == hook.password
 }
