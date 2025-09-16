09f70761de3340b1bc98402c99a4767b6f2e6032
pchchv <ipchchv@gmail.com>
2025-09-16 11:13:49 +0300

    test(azure): add webhook parsing tests for multiple Azure DevOps events - Cover build.complete, git.pullrequest.{created,merged,updated}, and git.push events - Validate parsing logic with real JSON payloads from testdata - Ensure correct event type mapping and no parsing errors

--- azure/azure_test.go
+++ azure/azure_test.go
@@ -7,9 +7,11 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"os"
+	"reflect"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 const virtualDir = "/webhooks"
@@ -26,6 +28,77 @@ func TestMain(m *testing.M) {
 
 	os.Exit(m.Run())
 	// teardown
+}
+
+func TestWebhooks(t *testing.T) {
+	assert := require.New(t)
+	tests := []struct {
+		name     string
+		event    Event
+		typ      interface{}
+		filename string
+		headers  http.Header
+	}{
+		{
+			name:     "build.complete",
+			event:    BuildCompleteEventType,
+			typ:      BuildCompleteEvent{},
+			filename: "../testdata/azuredevops/build.complete.json",
+		},
+		{
+			name:     "git.pullrequest.created",
+			event:    GitPullRequestCreatedEventType,
+			typ:      GitPullRequestEvent{},
+			filename: "../testdata/azuredevops/git.pullrequest.created.json",
+		},
+		{
+			name:     "git.pullrequest.merged",
+			event:    GitPullRequestMergedEventType,
+			typ:      GitPullRequestEvent{},
+			filename: "../testdata/azuredevops/git.pullrequest.merged.json",
+		},
+		{
+			name:     "git.pullrequest.updated",
+			event:    GitPullRequestUpdatedEventType,
+			typ:      GitPullRequestEvent{},
+			filename: "../testdata/azuredevops/git.pullrequest.updated.json",
+		},
+		{
+			name:     "git.push",
+			event:    GitPushEventType,
+			typ:      GitPushEvent{},
+			filename: "../testdata/azuredevops/git.push.json",
+		},
+	}
+
+	for _, tt := range tests {
+		tc := tt
+		client := &http.Client{}
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			payload, err := os.Open(tc.filename)
+			assert.NoError(err)
+			defer func() {
+				_ = payload.Close()
+			}()
+
+			var parseError error
+			var results interface{}
+			server := newServer(func(w http.ResponseWriter, r *http.Request) {
+				results, parseError = hook.Parse(r, tc.event)
+			})
+			defer server.Close()
+			req, err := http.NewRequest(http.MethodPost, server.URL+virtualDir, payload)
+			assert.NoError(err)
+			req.Header.Set("Content-Type", "application/json")
+
+			resp, err := client.Do(req)
+			assert.NoError(err)
+			assert.Equal(http.StatusOK, resp.StatusCode)
+			assert.NoError(parseError)
+			assert.Equal(reflect.TypeOf(tc.typ), reflect.TypeOf(results))
+		})
+	}
 }
 
 func TestBasicAuth(t *testing.T) {
