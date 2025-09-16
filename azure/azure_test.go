692ddf9b100fe691a52b58128ec7f336b3c79b28
pchchv <ipchchv@gmail.com>
2025-09-16 11:02:09 +0300

    test(azure): add TestMain setup for Webhook

--- azure/azure_test.go
+++ azure/azure_test.go
@@ -1,1 +1,21 @@
 package azure
+
+import (
+	"log"
+	"os"
+	"testing"
+)
+
+var hook *Webhook
+
+func TestMain(m *testing.M) {
+	// setup
+	var err error
+	hook, err = New()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	os.Exit(m.Run())
+	// teardown
+}
