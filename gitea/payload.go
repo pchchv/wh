5c154c65fc69d4da7dcc01bdd6e754bcdeb7f4b1
pchchv <ipchchv@gmail.com>
2025-09-17 09:33:26 +0300

    feat(gitea): add hook action type definitions - Introduce HookRepoAction for repository actions - Add HookIssueAction for issue-related actions - Define HookReleaseAction for release events - Define HookIssueCommentAction for issue comment events

--- gitea/payload.go
+++ gitea/payload.go
@@ -5,3 +5,15 @@ type PusherType string
 
 // StateType issue state type.
 type StateType string
+
+// HookRepoAction an action that happens to a repo.
+type HookRepoAction string
+
+// HookIssueAction FIXME.
+type HookIssueAction string
+
+// HookReleaseAction defines hook release action type.
+type HookReleaseAction string
+
+// HookIssueCommentAction defines hook issue comment action.
+type HookIssueCommentAction string
