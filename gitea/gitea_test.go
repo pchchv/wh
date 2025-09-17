421842b6b9323dbaae58b01bb97824883f0628c2
pchchv <ipchchv@gmail.com>
2025-09-17 12:12:36 +0300

    test(gitea): add TestMain setup for Webhook tests - Initialize global Webhook instance with secret for testing - Ensure proper setup before running all test cases

--- gitea/gitea_test.go
+++ gitea/gitea_test.go
@@ -1,11 +1,28 @@
 package gitea
 
 import (
+	"log"
 	"net/http"
 	"net/http/httptest"
+	"os"
+	"testing"
 )
 
 const path = "/webhooks"
+
+var hook *Webhook
+
+func TestMain(m *testing.M) {
+	// setup
+	var err error
+	hook, err = New(Options.Secret("IsWishesWereHorsesWedAllBeEatingSteak!"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	os.Exit(m.Run())
+	// teardown
+}
 
 func newServer(handler http.HandlerFunc) *httptest.Server {
 	mux := http.NewServeMux()
