f8208f6e255575aaef26321e3822132af94acc41
pchchv <ipchchv@gmail.com>
2025-09-17 11:58:00 +0300

    feat(gitea): add Event type with initial BuildEvent constant

--- gitea/gitea.go
+++ gitea/gitea.go
@@ -1,1 +1,26 @@
 package gitea
+
+const (
+	// Gitea hook types.
+	ForkEvent                 Event = "fork"
+	PushEvent                 Event = "push"
+	CreateEvent               Event = "create"
+	DeleteEvent               Event = "delete"
+	IssuesEvent               Event = "issues"
+	ReleaseEvent              Event = "release"
+	RepositoryEvent           Event = "repository"
+	IssueLabelEvent           Event = "issue_label"
+	IssueAssignEvent          Event = "issue_assign"
+	PullRequestEvent          Event = "pull_request"
+	IssueCommentEvent         Event = "issue_comment"
+	IssueMilestoneEvent       Event = "issue_milestone"
+	PullRequestSyncEvent      Event = "pull_request_sync"
+	PullRequestLabelEvent     Event = "pull_request_label"
+	PullRequestAssignEvent    Event = "pull_request_assign"
+	PullRequestReviewEvent    Event = "pull_request_review"
+	PullRequestCommentEvent   Event = "pull_request_comment"
+	PullRequestMilestoneEvent Event = "pull_request_milestone"
+)
+
+// Event defines a GitLab hook event type by the X-Gitlab-Event Header.
+type Event string
