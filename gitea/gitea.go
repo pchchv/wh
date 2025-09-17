3e0badca94ef046417937734438288d175aa39a0
pchchv <ipchchv@gmail.com>
2025-09-17 11:59:05 +0300

    feat(gitea): add Webhook struct - Define Webhook with secret field to manage event processing - Provide base for implementing webhook handling methods

--- gitea/gitea.go
+++ gitea/gitea.go
@@ -24,3 +24,8 @@ const (
 
 // Event defines a GitLab hook event type by the X-Gitlab-Event Header.
 type Event string
+
+// Webhook instance contains all methods needed to process events.
+type Webhook struct {
+	secret string
+}
