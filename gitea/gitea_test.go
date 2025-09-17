baf091f6c665f1b953894b9c4eeb6f44d5fb01b3
pchchv <ipchchv@gmail.com>
2025-09-17 11:51:48 +0300

    test(gitea): implement helper server for webhook handler tests

--- gitea/gitea_test.go
+++ gitea/gitea_test.go
@@ -1,1 +1,14 @@
 package gitea
+
+import (
+	"net/http"
+	"net/http/httptest"
+)
+
+const path = "/webhooks"
+
+func newServer(handler http.HandlerFunc) *httptest.Server {
+	mux := http.NewServeMux()
+	mux.HandleFunc(path, handler)
+	return httptest.NewServer(mux)
+}
